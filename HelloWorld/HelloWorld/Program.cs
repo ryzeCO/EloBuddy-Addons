919bbe46aedb1f35502084db984729f7f59c8da7
Cerie <bad.cerie@yandex.com>
2015-12-05 11:53:12 -0600

    OnLoadingComplete
    Add start method

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using SharpDX;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// Start program when the game loading is complete
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Start;
+        }
+    
+        private static void Start(EventArgs args){ 
+            Chat.Print("Hello World!");
+        }
+    }
+}
